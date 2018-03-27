b1dce00301139ad5dc9bcc65853ab52c35b7b664
Sochinda <sochinda@gmail.com>
2018-03-27 09:07:02 +0000

    Init

---
+++ surveyParseServer/index.js
@@ -0,0 +1,61 @@
+// Example express application adding the parse-server module to expose Parse
+// compatible API routes.
+
+var express = require('express');
+var ParseServer = require('parse-server').ParseServer;
+var path = require('path');
+
+const db = process.env.DATABASE
+const databaseUri = db + "surveyDb" || process.env.MONGODB_URI;
+const serverURL = process.env.SERVER_URL_SURVEY || 'http://localhost:1337/api';
+const appId = process.env.APP_ID_SURVEY || 'myAppId';
+const masterKey = process.env.MASTER_KEY_SURVEY || '';
+
+console.log(db);
+
+if (!databaseUri) {
+  console.log('DATABASE_URI not specified, falling back to localhost.');
+}
+
+var api = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId,
+  masterKey, //Add your master key here. Keep it secret!
+  serverURL,  // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  }
+});
+// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// If you wish you require them, you can set them as options in the initialization above:
+// javascriptKey, restAPIKey, dotNetKey, clientKey
+
+var app = express();
+
+// Serve static assets from the /public folder
+//app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// Serve the Parse API on the /parse URL prefix
+var mountPath = '/' + process.env.MOUNT_SURVEY || '/parse';
+app.use(mountPath, api);
+
+// Parse Server plays nicely with the rest of your web routes
+app.get('/', function(req, res) {
+  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+});
+
+// There will be a test page available on the /test path of your server url
+// Remove this before launching your app
+app.get('/test', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/test.html'));
+});
+
+var port = process.env.PORT_SURVEY || 1337;
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
+    console.log('parse-server-example running on port ' + port + '.');
+});
+
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer);
